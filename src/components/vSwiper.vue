13442dd16a2589349f0139bc227c77c2b92e2601
icecrying <1109339936@qq.com>
2019-11-09 18:52:45 +0800

    魅族商城

---
+++ src/components/vSwiper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+$END$
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vSwiper',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
